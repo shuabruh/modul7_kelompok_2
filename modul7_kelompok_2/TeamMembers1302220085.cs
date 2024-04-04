d06c73711fc3f3dfea07e8617f779feb3aa7ef18
myousefg <myousefg@gmail.com>
2024-04-04 11:10:43 +0700

    menambahkan class team members

---
+++ modul7_kelompok_2/TeamMembers1302220085.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace teammembers
+{
+    public class teamMembers1302220085
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public string nim { get; set; }
+    }
+}
