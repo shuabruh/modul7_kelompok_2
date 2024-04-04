093721417fe5008069cbb7a0fe55879e65d90b84
aufa <abdillahaufa79@gmail.com>
2024-04-04 10:54:33 +0700

    “menambahkan class DataMahasiswa_1302220131"

---
+++ modul7_kelompok_2/DataMahasiswa1302220131.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DataMahasiswa
+{
+    public class Datamahasiswa_1302220131
+    {
+        public class nama
+        {
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+        }
+        public class address
+        {
+            public string streetAddress { get; set; }
+            public string city { get; set; }
+            public string state { get; set; }
+        }
+
+        public class courses
+        {
+            public string code { get; set; }
+            public string name { get; set; }
+        }
+        public class data
+        {
+            public nama nama { get; set; }
+            public string gender { get; set; }
+            public int age { get; set; }
+            public address address { get; set; }
+            public courses courses { get; set; }
+        }
+    }
+}
--- modul7_kelompok_2/Program.cs
+++ modul7_kelompok_2/Program.cs
@@ -1,7 +1,22 @@
-﻿internal class Program
+﻿using System.Text.Json;
+using DataMahasiswa;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        readJSON();
+    }
+    private static void readJSON()
+    {
+        string filepath = Path.Combine(@"C:\Users\delll\Downloads\New folder\jurnal7_1_1302220131.json");
+        string jsonFile = File.ReadAllText(filepath);
+        Datamahasiswa_1302220131 data = JsonSerializer.Deserialize<Datamahasiswa_1302220131>(jsonFile);
+
+        Console.WriteLine(value: "Nama : " + data.nama.firstName + " " + data.nama.lastName);
+        Console.WriteLine("Gender: " + data.gender);
+        Console.WriteLine("Age: " + data.gender);
+        Console.WriteLine(value: "Address: " + data.address.streetAddress + " " + data.address.city + " " + data.address.state);
+        Console.WriteLine(value: "Courses: " + data.courses.code + " " + data.courses.name);
     }
 }
