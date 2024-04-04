2a768a67950330fd4d05a9ce82d96cbc11db14d7
myousefg <myousefg@gmail.com>
2024-04-04 11:02:39 +0700

    menambahkan class data mahasiswa

---
+++ modul7_kelompok_2/DataMahasiswa1302220085.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DataMahasiswa
+{
+    public class DataMahasiswa1302220085
+    {
+        
+        public class address
+        {
+            public string streetAddress { get; set; }
+            public string city { get; set; }
+            public string state { get; set; }
+        }
+
+        public class courses
+        {
+            public string code { get; set; }
+            public string name { get; set; }
+        }
+
+        public class data
+        {
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public string gender { get; set; }
+            public int age { get; set; }
+            public address address { get; set; }
+            public List<courses> mataKuliah { get; set; }
+
+        }
+    }
+}
+
--- modul7_kelompok_2/Program.cs
+++ modul7_kelompok_2/Program.cs
@@ -1,7 +1,35 @@
-﻿internal class Program
+﻿using System.Text.Json;
+using DataMahasiswa;
+
+internal class Program
 {
-    private static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        ReadJSON();
+    }
+
+    public static void ReadJSON()
+    {
+        string filepath = Path.Combine(@"D:\1302220085_MOD7_JURNAL_RMR\modul7_kelompok_2\modul7_kelompok_2\jurnal7_1_1302220085.json");
+        string jsonfile = File.ReadAllText(filepath);
+        DataMahasiswa1302220085.data data = JsonSerializer.Deserialize<DataMahasiswa1302220085.data>(jsonfile);
+        List<DataMahasiswa1302220085.courses> listMK = new List<DataMahasiswa1302220085.courses>();
+
+        Console.WriteLine("Nama :   " + data.firstName + " " + data.lastName);
+        Console.WriteLine("Gender : " + data.gender);
+        Console.WriteLine("Umur :   " + data.age);
+        Console.WriteLine("Alamat : " + data.address.streetAddress);
+        Console.WriteLine("         " + data.address.city);
+        Console.WriteLine("         " + data.address.state);
+
+        foreach (DataMahasiswa1302220085.courses objMK in listMK)
+        {
+            listMK.Add(objMK);
+        }
+
+        for (int i = 0; i < listMK.Count; i++)
+        {
+            Console.WriteLine("Mata Kuliah : " + data.mataKuliah);
+        }
     }
 }
