4c9d26744d61e6656cfac00144c545ec283d15d2
Irvandzawinnuha <72366333+Irvandzawinnuha@users.noreply.github.com>
2024-04-04 11:44:13 +0700

    menambahkan dataMahasiswa

---
+++ modul7/modul7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using DataMahasiswa;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        readJSON();
+    }
+    private static void readJSON()
+    {
+        string filepath = Path.Combine("D:\\matkul jurusan rpl telu\\Semester 4\\praktikum kpl\\jurnal7");
+        string jsonFile = File.ReadAllText(filepath);
+        Datamahasiswa_1302223076 data = JsonSerializer.Deserialize<Datamahasiswa_1302223076>(jsonFile);
+
+        Console.WriteLine(value: "Nama : " + data.nama.firstName + " " + data.nama.lastName);
+        Console.WriteLine("Gender: " + data.gender);
+        Console.WriteLine("Age: " + data.gender);
+        Console.WriteLine(value: "Address: " + data.address.streetAddress + " " + data.address.city + " " + data.address.state);
+        Console.WriteLine(value: "Courses: " + data.courses.code + " " + data.courses.name);
+    }
+}
